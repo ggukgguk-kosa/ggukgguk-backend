494ddd8d8256861d006f18203b6b884f6da10749
0tak2 <0tak2.code@gmail.com>
2023-04-12 18:20:26 +0900

    4/12 18:20 게시글 리스트 조회 구현

--- ggukgguk/src/main/java/com/ggukgguk/api/admin/dao/AdminDaoImpl.java
+++ ggukgguk/src/main/java/com/ggukgguk/api/admin/dao/AdminDaoImpl.java
@@ -1,10 +1,13 @@
 package com.ggukgguk.api.admin.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.ggukgguk.api.admin.vo.Notice;
+import com.ggukgguk.api.admin.vo.NoticeOption;
 
 @Repository
 public class AdminDaoImpl implements AdminDao {
@@ -19,6 +22,11 @@ public class AdminDaoImpl implements AdminDao {
 		if (affectedRow != 1) {
 			throw new Exception();
 		}
+	}
+
+	@Override
+	public List<Notice> selectNoticeList(NoticeOption option) {
+		return session.selectList("com.ggukgguk.api.Notice.selectList", option);
 	}
 
 }
--- ggukgguk/src/main/java/com/ggukgguk/api/admin/service/AdminService.java
+++ ggukgguk/src/main/java/com/ggukgguk/api/admin/service/AdminService.java
@@ -1,8 +1,12 @@
 package com.ggukgguk.api.admin.service;
 
+import java.util.List;
+
 import com.ggukgguk.api.admin.vo.Notice;
+import com.ggukgguk.api.admin.vo.NoticeOption;
 
 public interface AdminService {
 
-	public void addNotice(Notice notice);
+	public boolean addNotice(Notice notice);
+	public List<Notice> getNoticeList(NoticeOption option);
 }
